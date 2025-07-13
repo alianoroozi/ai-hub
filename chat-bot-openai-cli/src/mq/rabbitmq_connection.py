ca4584f72b764dac79dd58dc5ebba3cd18782002
Ali Noroozi <aliasghar.noroozi@gmail.com>
2025-07-13 14:19:17 -0400

    Add rabbitmq connection

---
+++ chat-bot-openai-cli/src/mq/rabbitmq_connection.py
@@ -0,0 +1,75 @@
+from typing import Self, Optional
+
+import pika
+
+from config import settings
+from utils.logger import get_logger
+
+logger = get_logger(__file__)
+
+
+class RabbitMQConnection:
+    """
+    Singleton class to manage RabbitMQ connection.
+    """
+
+    _instance = None
+
+    def __new__(cls, *args, **kwargs) -> Self:
+        if not cls._instance:
+            cls._instance = super().__new__(cls, *args, **kwargs)
+        return cls._instance
+
+    def __init__(
+        self,
+        host: Optional[str] = None,
+        port: Optional[int] = None,
+        username: Optional[str] = None,
+        password: Optional[str] = None,
+        virtual_host: str = "/",
+        fail_silently: bool = False,
+        **kwargs,
+    ) -> None:
+        self.host = host or settings.RABBITMQ_HOST
+        self.port = port or settings.RABBITMQ_PORT
+        self.username = username or settings.RABBITMQ_DEFAULT_USERNAME
+        self.password = password or settings.RABBITMQ_DEFAULT_PASSWORD
+        self.virtual_host = virtual_host
+        self.fail_silently = fail_silently
+        self._connection = None
+
+    def __enter__(self):
+        self.connect()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.close()
+
+    def connect(self):
+        try:
+            credentials = pika.PlainCredentials(self.username, self.password)
+            self._connection = pika.BlockingConnection(
+                pika.ConnectionParameters(
+                    host=self.host,
+                    port=self.port,
+                    virtual_host=self.virtual_host,
+                    credentials=credentials,
+                )
+            )
+        except pika.exceptions.AMQPConnectionError as e:
+            logger.exception("Failed to connect to RabbitMQ:")
+            if not self.fail_silently:
+                raise e
+
+    def is_connected(self) -> bool:
+        return self._connection is not None and self._connection.is_open
+
+    def get_channel(self):
+        if self.is_connected():
+            return self._connection.channel()
+
+    def close(self):
+        if self.is_connected():
+            self._connection.close()
+            self._connection = None
+            logger.info("Closed RabbitMQ connection")
